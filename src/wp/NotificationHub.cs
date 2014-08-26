df32be076dcba5d9d8324cfcdd7bc770b75648fe
PhonegapProjects <projects.phonegap.dev3@gmail.com>
2014-08-26 12:33:19 +0530

    Added tag for Windows Phone

--- src/wp/NotificationHub.cs
+++ src/wp/NotificationHub.cs
@@ -42,6 +42,7 @@ namespace Cordova.Extension.Commands
 
                 var notificationHubPath = args[0];
                 var connectionString = args[1];
+                var userId = args[3];
                 this.pushNotificationCallback = args[2];
 
                 if (string.IsNullOrEmpty(notificationHubPath))
@@ -66,17 +67,17 @@ namespace Cordova.Extension.Commands
                 if (channel == null)
                 {
                     channel = new HttpNotificationChannel(PluginChannelId);
-                    channel.ChannelUriUpdated += (o, res) => CompleteApplicationRegistration(res.ChannelUri.ToString(), notificationHubPath, connectionString);
+                    channel.ChannelUriUpdated += (o, res) => CompleteApplicationRegistration(res.ChannelUri.ToString(), notificationHubPath, connectionString, userId);
                     channel.Open();
                     channel.BindToShellToast();
                 }
                 else
                 {
-                    CompleteApplicationRegistration(channel.ChannelUri.ToString(), notificationHubPath, connectionString);
+                    CompleteApplicationRegistration(channel.ChannelUri.ToString(), notificationHubPath, connectionString, userId);
                 }
 
                 channel.ShellToastNotificationReceived += PushChannel_ShellToastNotificationReceived;
-                
+
             }
             catch (Exception ex)
             {
@@ -120,13 +121,13 @@ namespace Cordova.Extension.Commands
             }
         }
 
-        private async void CompleteApplicationRegistration(string channelUri, string notificationHubPath, string connectionString)
+        private async void CompleteApplicationRegistration(string channelUri, string notificationHubPath, string connectionString, string userId)
         {
             try
             {
                 var hub = new Microsoft.WindowsAzure.Messaging.NotificationHub(notificationHubPath, connectionString);
-                var registration = await hub.RegisterNativeAsync(channelUri);
-                
+                var registration = await hub.RegisterNativeAsync(channelUri, new string[] { userId });
+
                 var regInfo = new RegisterResult();
                 regInfo.RegistrationId = registration.RegistrationId;
                 regInfo.ChannelUri = registration.ChannelUri;
@@ -142,7 +143,7 @@ namespace Cordova.Extension.Commands
 
         void PushChannel_ShellToastNotificationReceived(object sender, NotificationEventArgs e)
         {
-            
+
             // if there is no js handler
             if (this.pushNotificationCallback == null) return;
 
